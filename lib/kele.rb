6b04e0aa7bd87d2d7d965dd2c673868cdc8a15ac
David Chavez <dvcv1992@gmail.com>
2017-11-29 17:14:57 -0800

    More user friendly

--- lib/kele.rb
+++ lib/kele.rb
@@ -9,19 +9,18 @@ class Kele
     @auth = {email: email, password: password}
   end
 
-  def post_response
-    if valid_credentials?
-      response
-    else
-      response["message"]
-    end
-  end
-
   def auth_token
     if valid_credentials?
       response["auth_token"]
     else
-      response["message"]
+      p  response["message"]
+      while !valid_credentials?
+        p "Enter Valid Email"
+        @auth[:email] = gets.chomp
+        p "Enter Valid Password"
+        @auth[:password] = gets.chomp
+        return self.auth_token
+      end
     end
   end
 
